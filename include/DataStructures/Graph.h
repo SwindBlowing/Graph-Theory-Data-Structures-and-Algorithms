5d80ef7dbef255c8c4b0969f73826210a787584d
zyb1029 <zyb@smail.nju.edu.cn>
2022-09-08 23:38:23 +0800

    update 'Graph.h'

--- include/DataStructures/Graph.h
+++ include/DataStructures/Graph.h
@@ -11,18 +11,18 @@ class Graph {
  public:
   bool AddVertex(int vertex);
   bool RemoveVertex(int vertex);
-  virtual bool AddEdge(int vertex1, int vertex2);
-  virtual bool RemoveEdge(int vertex1, int vertex2);
+  bool AddEdge(int vertex1, int vertex2);
+  bool RemoveEdge(int vertex1, int vertex2);
  public:
   int CountVertices() const;
-  virtual int CountEdges() const;
+  int CountEdges() const;
   bool ContainsVertex(int vertex) const;
   bool ContainsEdge(int vertex1, int vertex2) const;
   std::vector<int> GetVertices() const;
   std::vector<Edge> GetEdges() const;
   std::vector<Edge> GetIncomingEdges(int vertex) const;
   std::vector<Edge> GetOutgoingEdges(int vertex) const;
-  virtual int GetDegree(int vertex) const;
+  int GetDegree(int vertex) const;
   std::vector<int> GetNeighbors(int vertex) const;
 };
 
