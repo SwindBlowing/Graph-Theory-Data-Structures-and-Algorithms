19f97d0f96a070bd5a7e0fb9a4982c986271fc39
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-10 00:09:25 +0800

    compile

--- source/DataStructures/Graph.cpp
+++ source/DataStructures/Graph.cpp
@@ -47,11 +47,10 @@ bool Graph::AddEdge(int vertex1, int vertex2)
         return 0;
     int id1 = it1->second, id2 = it2->second;
     std::map <int, std::vector<Edge>>::iterator it = edges.find(id1);
-    std::vector<Edge>::iterator t;
-    //t = std::find(it->second.begin(), it->second.end(), e);
-    for (t = it->second.begin(); t != it->second.end(); t++)
-        if (t->GetDestination() == vertex2) return 0;
-    //if (t != it->second.end()) return 0;
+    std::vector<Edge>::iterator t = std::find(it->second.begin(), it->second.end(), e);
+    //for (t = it->second.begin(); t != it->second.end(); t++)
+    //    if (t->GetDestination() == vertex2) return 0;
+    if (t != it->second.end()) return 0;
     it->second.push_back(e);
     outdex[id1]++; index[id2]++;
     edgeNum++;
