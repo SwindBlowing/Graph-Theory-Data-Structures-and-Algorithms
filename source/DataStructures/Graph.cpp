4bd3c596c9dfd51bca60c905ff38d476f1a16e86
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-10 10:40:17 +0800

    compile

--- source/DataStructures/Graph.cpp
+++ source/DataStructures/Graph.cpp
@@ -34,7 +34,7 @@ bool Graph::RemoveVertex(int vertex)
         if (nex != it_in->second.end()) nex++;
     }
     auto it_out = outEdges.find(nowId);
-    auto nex = it_out->second.begin(); nex++;
+    ex = it_out->second.begin(); nex++;
     for (auto t = it_out->second.begin(); t != it_out->second.end();)
     {
         RemoveEdge(t->GetSource(), t->GetDestination());
