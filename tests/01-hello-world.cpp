caf52390ec7b425e8dedd127421aa701b73090f0
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-15 20:56:12 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -25,10 +25,10 @@ int main()
     assert(g.ContainsEdge(1, 2) == true);
     assert(g.ContainsEdge(2, 5) == false);
 
-    //assert(g.GetVertices().size() == g.CountVertices()); // 4
-    //assert(g.GetEdges().size() == 2);
-    //assert(g.GetIncomingEdges(1).size() == 0);
-    //assert(g.GetOutgoingEdges(1).size() == 2); // {{1, 2}, {1, 3}}
+    assert(g.GetVertices().size() == g.CountVertices()); // 4
+    assert(g.GetEdges().size() == 2);
+    assert(g.GetIncomingEdges(1).size() == 0);
+    assert(g.GetOutgoingEdges(1).size() == 2); // {{1, 2}, {1, 3}}
     assert(g.GetDegree(1) == 2);
     assert(g.GetNeighbors(1).size() == 2); // {2, 3}
     assert(g.GetNeighbors(2).size() == 0);
