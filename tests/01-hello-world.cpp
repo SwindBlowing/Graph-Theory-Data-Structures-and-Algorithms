ef289982fc5c19991f182f9c8c3eda5f8a9c23a7
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-15 21:20:11 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -19,14 +19,14 @@ int main()
     assert(g.ContainsVertex(4) == false);
     assert(g.RemoveVertex(5) == false);
 
-    assert(g.AddEdge(1, 2, 1) == true);
+    assert(g.AddEdge(1, 2, 3) == true);
     assert(g.AddEdge(1, 3, 2) == true);
     assert(g.AddEdge(2, 5, 3) == false); // 5号点不存在
     assert(g.AddEdge(1, 2, 3) == false);
     assert(g.ContainsEdge(1, 2) == true);
     assert(g.ContainsEdge(2, 5) == false);
 
-    assert(g.GetWeight(1, 2) == 1);
+    assert(g.GetWeight(1, 2) == 3);
     printf("%d", g.GetWeight(1, 3));
     //assert(g.GetWeight(1, 3) == 2);
     /*assert(g.GetWeight(1, 2) == 1);
