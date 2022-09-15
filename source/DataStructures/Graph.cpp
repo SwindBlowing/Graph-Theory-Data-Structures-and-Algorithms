a87bdc4fe7cd2ffbec3447623f45327bb8763506
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-15 20:04:04 +0800

    compile

--- source/DataStructures/Graph.cpp
+++ source/DataStructures/Graph.cpp
@@ -24,13 +24,13 @@ bool Graph::RemoveVertex(int vertex)
     if (!ContainsVertex(vertex)) return 0;
     vertexNum--;
     auto it_in = inEdges.at(vertex);
-    for (auto t = it_in->second.begin(); t != it_in->second.end(); t++)
+    for (auto t = it_in.begin(); t != it_in.end(); t++)
         Pause.push_back(*t);
     for (auto t = Pause.begin(); t != Pause.end(); t++)
         RemoveEdge(t->GetSource(), t->GetDestination());
     Pause.clear();
-    auto it_out = outEdges.find(vertex);
-    for (auto t = it_out->second.begin(); t != it_out->second.end(); t++)
+    auto it_out = outEdges.at(vertex);
+    for (auto t = it_out.begin(); t != it_out.end(); t++)
         Pause.push_back(*t);
     for (auto t = Pause.begin(); t != Pause.end(); t++)
         RemoveEdge(t->GetSource(), t->GetDestination());
@@ -43,10 +43,10 @@ bool Graph::AddEdge(int vertex1, int vertex2)
     if (!ContainsVertex(vertex1) || !ContainsVertex(vertex2) || ContainsEdge(vertex1, vertex2))
         return 0;
     Edge e = Edge(vertex1, vertex2);
-    auto it_in = inEdges.find(vertex2);
-    auto it_out = outEdges.find(vertex1);
-    it_in->second.push_back(e);
-    it_out->second.push_back(e);
+    auto it_in = inEdges.at(vertex2);
+    auto it_out = outEdges.at(vertex1);
+    it_in.push_back(e);
+    it_out.push_back(e);
     outdex[vertex1]++; index[vertex2]++;
     edgeNum++;
     return 1;
@@ -57,14 +57,14 @@ bool Graph::RemoveEdge(int vertex1, int vertex2)
     if (!ContainsVertex(vertex1) || !ContainsVertex(vertex2) || !ContainsEdge(vertex1, vertex2))
         return 0;
     Edge e = Edge(vertex1, vertex2);
-    auto it_in = inEdges.find(vertex2);
-    auto it_out = outEdges.find(vertex1);
+    auto it_in = inEdges.at(vertex2);
+    auto it_out = outEdges.at(vertex1);
     std::vector<Edge>::iterator t;
-    t = std::find(it_in->second.begin(), it_in->second.end(), e);
-    it_in->second.erase(t);
-    t = std::find(it_out->second.begin(), it_out->second.end(), e);
-    assert(t != it_out->second.end());
-    it_out->second.erase(t);
+    t = std::find(it_in.begin(), it_in.end(), e);
+    it_in.erase(t);
+    t = std::find(it_out.begin(), it_out.end(), e);
+    assert(t != it_out.end());
+    it_out.erase(t);
     outdex[vertex1]--; index[vertex2]--;
     edgeNum--;
     return 1;
@@ -88,8 +88,8 @@ bool Graph::ContainsVertex(int vertex) const
 bool Graph::ContainsEdge(int vertex1, int vertex2) const
 {
     if (!ContainsVertex(vertex1) || !ContainsVertex(vertex2)) return 0;
-    auto it = outEdges.find(vertex1);
-    return std::find(it->second.begin(), it->second.end(), Edge(vertex1, vertex2)) != it->second.end();
+    auto it = outEdges.at(vertex1);
+    return std::find(it.begin(), it.end(), Edge(vertex1, vertex2)) != it.end();
 }
 
 std::vector<int> Graph::GetVertices() const
@@ -104,8 +104,8 @@ std::vector<Edge> Graph::GetEdges() const
 {
     std::vector <Edge> edges; edges.clear();
     for (auto it = vertices.begin(); it != vertices.end(); it++) {
-        auto t = outEdges.find(*it);
-        for (auto now = t->second.begin(); now != t->second.end(); now++)
+        auto t = outEdges.at(*it);
+        for (auto now = t.begin(); now != t.end(); now++)
             edges.push_back(*now);
     }
     return edges;
@@ -114,13 +114,13 @@ std::vector<Edge> Graph::GetEdges() const
 std::vector<Edge> Graph::GetIncomingEdges(int vertex) const
 {
     if (!ContainsVertex(vertex)) return {};
-    return inEdges.find(vertex)->second;
+    return inEdges.at(vertex);
 }
 
 std::vector<Edge> Graph::GetOutgoingEdges(int vertex) const
 {
     if (!ContainsVertex(vertex)) return {};
-    return outEdges.find(vertex)->second;
+    return outEdges.at(vertex);
 }
 
 int Graph::GetDegree(int vertex) const
@@ -133,8 +133,8 @@ std::vector<int> Graph::GetNeighbors(int vertex) const
 {
     if (!ContainsVertex(vertex)) return {};
     std::vector <int> neighbors; neighbors.clear();
-    auto it = outEdges.find(vertex);
-    for (auto t = it->second.begin(); t != it->second.end(); t++)
+    auto it = outEdges.at(vertex);
+    for (auto t = it.begin(); t != it.end(); t++)
         neighbors.push_back(t->GetDestination());
     return neighbors;
 }
