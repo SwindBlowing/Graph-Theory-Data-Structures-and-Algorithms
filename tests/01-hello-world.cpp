a15d5ebba94e1da4dcd9159996ecd6fe477d4568
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-15 20:29:04 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-    WeightedGraph g; // 创建一张新的图
+    Graph g; // 创建一张新的图
 
     assert(g.AddVertex(1) == true);
     assert(g.AddVertex(2) == true);
@@ -19,16 +19,16 @@ int main()
     assert(g.ContainsVertex(4) == false);
     assert(g.RemoveVertex(5) == false);
 
-    assert(g.AddEdge(1, 2, 1) == true);
-    assert(g.AddEdge(1, 3, 1) == true);
-    assert(g.AddEdge(2, 5, 1) == false); // 5号点不存在
+    assert(g.AddEdge(1, 2) == true);
+    assert(g.AddEdge(1, 3) == true);
+    assert(g.AddEdge(2, 5) == false); // 5号点不存在
     assert(g.ContainsEdge(1, 2) == true);
     assert(g.ContainsEdge(2, 5) == false);
 
-    //assert(g.GetVertices().size() == g.CountVertices()); // 4
-    //assert(g.GetEdges().size() == 2);
-    //assert(g.GetIncomingEdges(1).size() == 0);
-    //assert(g.GetOutgoingEdges(1).size() == 2); // {{1, 2}, {1, 3}}
+    assert(g.GetVertices().size() == g.CountVertices()); // 4
+    assert(g.GetEdges().size() == 2);
+    assert(g.GetIncomingEdges(1).size() == 0);
+    assert(g.GetOutgoingEdges(1).size() == 2); // {{1, 2}, {1, 3}}
     assert(g.GetDegree(1) == 2);
     assert(g.GetNeighbors(1).size() == 2); // {2, 3}
     assert(g.GetNeighbors(2).size() == 0);
