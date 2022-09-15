799d5e163c82c6fa76188a595634675ff7b7c620
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-15 20:19:01 +0800

    compile

---
+++ include/DataStructures/WeightedGraph.h
@@ -0,0 +1,44 @@
+#ifndef WEIGHTEDGRAPH_H
+#define WEIGHTEDGRAPH_H
+
+#include <vector>
+#include <map>
+#include <set>
+#include <utility>
+#include <algorithm>
+#include <assert.h>
+#include <iostream>
+#include <DataStructures/WeightedEdge.h>
+#include <DataStructures/Graph.h>
+#define MAXN 1005
+
+class WeightedGraph : public Graph {
+ public:
+  WeightedGraph() {}
+  ~WeightedGraph() {}
+ public:
+  //bool AddVertex(int vertex);
+  //bool RemoveVertex(int vertex);
+  bool AddEdge(int vertex1, int vertex2, int weight);
+  //bool RemoveEdge(int vertex1, int vertex2);
+ public:
+  //int CountVertices() const;
+  //int CountEdges() const;
+  //bool ContainsVertex(int vertex) const;
+  //bool ContainsEdge(int vertex1, int vertex2) const;
+  int GetWeight(int vertex1, int vertex2) const;
+  //std::vector<int> GetVertices() const;
+  std::vector<WeightedEdge> GetEdges() const;
+  std::vector<WeightedEdge> GetIncomingEdges(int vertex) const;
+  std::vector<WeightedEdge> GetOutgoingEdges(int vertex) const;
+  //int GetDegree(int vertex) const;
+  //std::vector<int> GetNeighbors(int vertex) const;
+
+ private:
+  //std::set <int> vertices;
+  std::map <int, std::vector<WeightedEdge>> inEdges, outEdges;
+  //std::map <int, int> index, outdex;
+  //int vertexNum = 0, edgeNum = 0;
+};
+
+#endif
