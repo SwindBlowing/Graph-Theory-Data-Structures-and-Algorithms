630a644685cb654ed37d16a7090c0fe04bbbcc5f
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-22 19:55:12 +0800

    compile

--- source/DataStructures/UndirectedGraph.cpp
+++ source/DataStructures/UndirectedGraph.cpp
@@ -1,6 +1,6 @@
 #include <DataStructures/UndirectedGraph.h>
 
-/*bool UndirectedGraph::AddEdge(int vertex1, int vertex2) 
+bool UndirectedGraph::AddEdge(int vertex1, int vertex2) 
 {
     if (!Graph::AddEdge(vertex1, vertex2)) return 0;
     if (vertex2 != vertex1) Graph::AddEdge(vertex2, vertex1);
@@ -8,7 +8,7 @@
     return 1;
 }
 
-bool UndirectedGraph::RemoveEdge(int vertex1, int vertex2) 
+/*bool UndirectedGraph::RemoveEdge(int vertex1, int vertex2) 
 {
     if (!Graph::RemoveEdge(vertex1, vertex2)) return 0;
     if (vertex2 != vertex1) Graph::RemoveEdge(vertex2, vertex1);
