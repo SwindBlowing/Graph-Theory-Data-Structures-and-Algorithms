7a4865ba43e04981f1a80760fa3b4edf0bfd1c9f
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-22 20:40:12 +0800

    compile

--- include/DataStructures/UndirectedGraph.h
+++ include/DataStructures/UndirectedGraph.h
@@ -20,17 +20,17 @@ class UndirectedGraph : public Graph {
   //bool AddVertex(int vertex);
   //bool RemoveVertex(int vertex);
   bool AddEdge(int vertex1, int vertex2) override;
-  ///bool RemoveEdge(int vertex1, int vertex2) override;
+  bool RemoveEdge(int vertex1, int vertex2) override;
  public:
   //int CountVertices() const;
-  ///int CountEdges() const override;
+  int CountEdges() const override;
   //bool ContainsVertex(int vertex) const;
   //bool ContainsEdge(int vertex1, int vertex2) const;
   //std::vector<int> GetVertices() const;
   //std::vector<Edge> GetEdges() const;
   //std::vector<Edge> GetIncomingEdges(int vertex) const;
   //std::vector<Edge> GetOutgoingEdges(int vertex) const;
-  ///int GetDegree(int vertex) const override;
+  int GetDegree(int vertex) const override;
   //std::vector<int> GetNeighbors(int vertex) const;
  private:
   std::map <int, int> selfLoop;
--- source/DataStructures/UndirectedGraph.cpp
+++ source/DataStructures/UndirectedGraph.cpp
@@ -2,13 +2,13 @@
 
 bool UndirectedGraph::AddEdge(int vertex1, int vertex2) 
 {
-    //if (!Graph::AddEdge(vertex1, vertex2)) return 0;
-    //if (vertex2 != vertex1) Graph::AddEdge(vertex2, vertex1);
-    //else selfLoop[vertex1]++;
+    if (!Graph::AddEdge(vertex1, vertex2)) return 0;
+    if (vertex2 != vertex1) Graph::AddEdge(vertex2, vertex1);
+    else selfLoop[vertex1]++;
     return 1;
 }
 
-/*bool UndirectedGraph::RemoveEdge(int vertex1, int vertex2) 
+bool UndirectedGraph::RemoveEdge(int vertex1, int vertex2) 
 {
     if (!Graph::RemoveEdge(vertex1, vertex2)) return 0;
     if (vertex2 != vertex1) Graph::RemoveEdge(vertex2, vertex1);
@@ -24,4 +24,4 @@ int UndirectedGraph::CountEdges() const
 int UndirectedGraph::GetDegree(int vertex) const
 {
     return Graph::GetDegree(vertex) + selfLoop[vertex];
-}*/
+}
