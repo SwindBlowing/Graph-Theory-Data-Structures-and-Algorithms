85653ecf693ed6754f5b453829c792704fafc45e
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-22 18:50:45 +0800

    compile

--- include/DataStructures/Graph.h
+++ include/DataStructures/Graph.h
@@ -18,15 +18,15 @@ class Graph {
  public:
   bool AddVertex(int vertex);
   bool RemoveVertex(int vertex);
-  bool AddEdge(int vertex1, int vertex2);
-  bool RemoveEdge(int vertex1, int vertex2);
+  virtual bool AddEdge(int vertex1, int vertex2);
+  virtual bool RemoveEdge(int vertex1, int vertex2);
  public:
   int CountVertices() const;
-  int CountEdges() const;
+  virtual int CountEdges() const;
   bool ContainsVertex(int vertex) const;
   bool ContainsEdge(int vertex1, int vertex2) const;
   std::vector<int> GetVertices() const;
-  std::vector<Edge> GetEdges() const;
+  virtual std::vector<Edge> GetEdges() const;
   std::vector<Edge> GetIncomingEdges(int vertex) const;
   std::vector<Edge> GetOutgoingEdges(int vertex) const;
   int GetDegree(int vertex) const;
