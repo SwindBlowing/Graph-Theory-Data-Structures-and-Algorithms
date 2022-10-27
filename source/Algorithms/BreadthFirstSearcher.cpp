b4156cc00c1e8a4bc3687f3ae162f3289305d5b9
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-10-27 22:55:54 +0800

    compile

--- source/Algorithms/BreadthFirstSearcher.cpp
+++ source/Algorithms/BreadthFirstSearcher.cpp
@@ -1,9 +1,12 @@
 #include <Algorithms/BreadthFirstSearcher.h>
 
+template <typename TGraph>
 static void BreadthFirstSearcher::VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action)
 {
 
 }
+
+template <typename TGraph>
 static std::optional<int> BreadthFirstSearcher::FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate)
 {
 
--- source/Algorithms/DepthFirstSearcher.cpp
+++ source/Algorithms/DepthFirstSearcher.cpp
@@ -1,9 +1,12 @@
 #include <Algorithms/DepthFirstSearcher.h>
 
+template <typename TGraph>
 static void DepthFirstSearcher::VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action)
 {
 
 }
+
+template <typename TGraph>
 static std::optional<int> DepthFirstSearcher::FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate)
 {
 
