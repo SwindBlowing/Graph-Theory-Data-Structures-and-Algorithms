6d8047dd7b725bf3395cad166cdf9b35a9341a24
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-10-27 23:11:54 +0800

    compile

--- source/Algorithms/DepthFirstSearcher.cpp
+++ source/Algorithms/DepthFirstSearcher.cpp
@@ -2,12 +2,16 @@
 #include <string.h>
 
 template <typename TGraph>
+void dfs(const TGraph *graph, int x, std::function<void(int)> action)
+{
+
+}
+
+template <typename TGraph>
 void DepthFirstSearcher<TGraph>::VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action)
 {
-	Graph = graph;
-	Action = action;
-
 	memset(vis, 0, sizeof(vis));
+	dfs(graph, start, action);
 }
 
 template <typename TGraph>
