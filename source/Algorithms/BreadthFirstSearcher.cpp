dca7e0a5525703d05b908bf812021bcf2f8c8703
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-10-27 22:58:08 +0800

    compile

--- source/Algorithms/BreadthFirstSearcher.cpp
+++ source/Algorithms/BreadthFirstSearcher.cpp
@@ -3,11 +3,11 @@
 template <typename TGraph>
 static void BreadthFirstSearcher::VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action)
 {
-
+	action(start);
 }
 
 template <typename TGraph>
 static std::optional<int> BreadthFirstSearcher::FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate)
 {
-
+	predicate(start);
 }
