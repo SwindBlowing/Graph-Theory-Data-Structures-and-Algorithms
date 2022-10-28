af8898b2f7f39fb62260c8b35932ef716b04d194
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-10-28 09:59:31 +0800

    compile

--- include/Algorithms/DepthFirstSearcher.h
+++ include/Algorithms/DepthFirstSearcher.h
@@ -11,66 +11,65 @@
 #include <functional>
 #include <optional>
 #include <map>
+#include <stack>
 
 template <typename TGraph>
-class DepthFirstSearcher {
- public:
-  static void VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action);
-  static std::optional<int> FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate);
+class DepthFirstSearcher
+{
+public:
+	static void VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action);
+	static std::optional<int> FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate);
 
- private:
-  static void dfsVisit(int x);
-  static std::optional<int> dfsFind(int x);
-
- private:
-  static std::map<int, int> vis;
-  static TGraph *Graph;
-  static std::function<void(int)> Action;
-  static std::function<bool(int)> Predicate;
+private:
+	static std::stack<int> S;
+	static std::map<int, int> vis;
 };
-
-template <typename TGraph>
-void DepthFirstSearcher<TGraph>::dfsVisit(int x)
-{
-	if (vis[x]) return ;
-	vis[x] = 1;
-	Action(x);
-	for (int i = 0; i < Graph->outedges[x].size(); i++) 
-		dfsVisit(Graph->outedges[x][i].destination);
-	return ;
-}
-
-template <typename TGraph>
-std::optional<int> DepthFirstSearcher<TGraph>::dfsFind(int x)
-{
-	if (vis[x]) return std::nullopt;
-	vis[x] = 1;
-	if (Predicate(x)) return x;
-	std::optional<int> now = std::nullopt;
-	for (int i = 0; i < Graph->outedges[x].size(); i++) {
-		now = dfsFind(Graph->outedges[x][i].destination);
-		if (now != std::nullopt) return now;
-	}
-	return std::nullopt;
-}
 
 template <typename TGraph>
 void DepthFirstSearcher<TGraph>::VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action)
 {
 	vis.clear();
-	Graph = graph;
-	Action = action;
-	dfsVisit(start);
-	return ;
+	while (!S.empty()) S.pop();
+	S.push(start);
+	vis[start] = 1;
+	int x;
+	while (!S.empty())
+	{
+		x = S.top();
+		S.pop();
+		action(x);
+		for (int i = 0; i < graph->outedges[x].size(); i++)
+			if (!vis[graph->outedges[x][i].destination])
+			{
+				vis[graph->outedges[x][i].destination] = 1;
+				S.push(graph->outedges[x][i].destination);
+			}
+	}
+	return;
 }
 
 template <typename TGraph>
 std::optional<int> DepthFirstSearcher<TGraph>::FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate)
 {
 	vis.clear();
-	Graph = graph;
-	Predicate = predicate;
-	return dfsFind(start);
+	while (!S.empty()) S.pop();
+	S.push(start);
+	vis[start] = 1;
+	int x;
+	while (!S.empty())
+	{
+		x = S.top();
+		S.pop();
+		if (predicate(x))
+			return x;
+		for (int i = 0; i < graph->outedges[x].size(); i++)
+			if (!vis[graph->outedges[x][i].destination])
+			{
+				vis[graph->outedges[x][i].destination] = 1;
+				S.push(graph->outedges[x][i].destination);
+			}
+	}
+	return std::nullopt;
 }
 
 #endif
