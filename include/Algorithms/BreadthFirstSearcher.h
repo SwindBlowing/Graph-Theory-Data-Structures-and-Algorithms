333361ab25e48a7a854953078a5e72592198a05a
zyb1029 <zyb@smail.nju.edu.cn>
2022-11-03 12:27:07 +0800

    update api for bfs and dfs

--- include/Algorithms/BreadthFirstSearcher.h
+++ include/Algorithms/BreadthFirstSearcher.h
@@ -8,7 +8,7 @@ template <typename TGraph>
 class BreadthFirstSearcher {
  public:
   static void VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action);
-  static std::optional<int> FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate);
+  static std::optional<int> TryFindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate);
 };
 
 #endif
