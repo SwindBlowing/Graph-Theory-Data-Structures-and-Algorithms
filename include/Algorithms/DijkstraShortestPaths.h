6ce2b1ee522391bc4450154e8a6928ec1388dd2a
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-04 16:28:15 +0800

    compile

--- include/Algorithms/DijkstraShortestPaths.h
+++ include/Algorithms/DijkstraShortestPaths.h
@@ -3,8 +3,12 @@
 
 #include <vector>
 #include <optional>
+#include <map>
+#include <queue>
 
 #include <Algorithms/ShortestPaths.h>
+#include <DataStructures/WeightedEdge.h>
+#define MAXN 1005
 
 template <template<typename> class TGraph, typename TValue>
 class DijkstraShortestPaths : public ShortestPaths<TGraph, TValue> {
@@ -13,11 +17,15 @@ class DijkstraShortestPaths : public ShortestPaths<TGraph, TValue> {
     typedef std::optional<TValue> (ShortestPaths<TGraph, TValue>::*fp_Dis)(int destination) const;
     typedef std::optional<std::vector<int>> (ShortestPaths<TGraph, TValue>::*fp_Path)(int destination) const;
 	DijkstraShortestPaths(const TGraph<TValue> *graph, int source);
-	~DijkstraShortestPaths();
+	~DijkstraShortestPaths() {};
   public:
 	bool HasPathTo(int destination) const;
 	std::optional<TValue> TryGetDistanceTo(int destination) const;
 	std::optional<std::vector<int>> TryGetShortestPathTo(int destination) const;
+  private:
+	std::map<int, bool> vis, reached;
+	std::map<int, TValue> dist;
+	std::priority_queue<std::pair<TValue, int>, std::vector<std::pair<TValue, int>>, std::greater<std::pair<TValue, int>>> Q;
 };
 
 template <template<typename> class TGraph, typename TValue>
@@ -26,12 +34,31 @@ DijkstraShortestPaths<TGraph, TValue>::DijkstraShortestPaths(const TGraph<TValue
 	this->fn_HasPathTo = (fp_Has)(&DijkstraShortestPaths::HasPathTo);
 	this->fn_TryGetDistanceTo = (fp_Dis)(&DijkstraShortestPaths::TryGetDistanceTo);
 	this->fn_TryGetShortestPathTo = (fp_Path)(&DijkstraShortestPaths::TryGetShortestPathTo);
-}
+	vis.clear(); reached.clear(); dist.clear();
+	while (!Q.empty()) Q.pop();
+	if (!graph->ContainsVertex(source)) return ;
 
-template <template<typename> class TGraph, typename TValue>
-DijkstraShortestPaths<TGraph, TValue>::~DijkstraShortestPaths()
-{
-
+	std::vector<WeightedEdge<TValue>> outEdges;
+	reached[source] = 1;
+	dist[source] = TValue();
+	Q.push({dist[source], source});
+	while (!Q.empty()) {
+		std::pair<TValue, int> now = Q.top();
+		Q.pop();
+		if (vis[now.second]) continue;
+		vis[now.second] = 1;
+		outEdges = graph->GetOutgoingEdges(now.second);
+		for (int i = 0; i < outEdges.size(); i++) {
+			int y = outEdges[i].GetDestination();
+			TValue w = outEdges[i].GetWeight();
+			if (!reached[y] || dist[y] > dist[now.second] + w) {
+				reached[y] = 1;
+				dist[y] = dist[now.second] + w;
+				Q.push({dist[y], y});
+			}
+		}
+			
+	}
 }
 
 template <template<typename> class TGraph, typename TValue>
