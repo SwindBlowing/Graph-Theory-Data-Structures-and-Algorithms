0bae3ab770d2bfae5df86bc27d67a5614a029c34
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-04 15:29:37 +0800

    compile

--- include/Algorithms/DijkstraShortestPaths.h
+++ include/Algorithms/DijkstraShortestPaths.h
@@ -7,7 +7,7 @@
 #include <Algorithms/ShortestPaths.h>
 
 template <template<typename> class TGraph, typename TValue>
-class DijkstraShortestPaths : public ShortestPaths<TGraph<TValue>, TValue> {
+class DijkstraShortestPaths : public ShortestPaths<TGraph, TValue> {
   public:
 	DijkstraShortestPaths(const TGraph<TValue> *graph, int source);
 	~DijkstraShortestPaths() override;
@@ -18,7 +18,7 @@ class DijkstraShortestPaths : public ShortestPaths<TGraph<TValue>, TValue> {
 }
 
 template <template<typename> class TGraph, typename TValue>
-DijkstraShortestPaths<TGraph<TValue>, TValue>::DijkstraShortestPaths(const TGraph<TValue> *graph, int source)
+DijkstraShortestPaths<TGraph, TValue>::DijkstraShortestPaths(const TGraph<TValue> *graph, int source)
 {
 	this->fn_HasPathTo = (pfn)(&DijkstraShortestPaths::HasPathTo);
 	this->fn_TryGetDistanceTo = (pfn)(&DijkstraShortestPaths::TryGetDistanceTo);
@@ -26,25 +26,25 @@ DijkstraShortestPaths<TGraph<TValue>, TValue>::DijkstraShortestPaths(const TGrap
 }
 
 template <template<typename> class TGraph, typename TValue>
-DijkstraShortestPaths<TGraph<TValue>, TValue>::~DijkstraShortestPaths()
+DijkstraShortestPaths<TGraph, TValue>::~DijkstraShortestPaths()
 {
 
 }
 
 template <template<typename> class TGraph, typename TValue>
-bool DijkstraShortestPaths<TGraph<TValue>, TValue>::HasPathTo(int destination) const
+bool DijkstraShortestPaths<TGraph, TValue>::HasPathTo(int destination) const
 {
 	return 1;
 }
 
 template <template<typename> class TGraph, typename TValue>
-std::optional<TValue> DijkstraShortestPaths<TGraph<TValue>, TValue>::TryGetDistanceTo(int destination) const
+std::optional<TValue> DijkstraShortestPaths<TGraph, TValue>::TryGetDistanceTo(int destination) const
 {
 	return std::nullopt;
 }
 
 template <template<typename> class TGraph, typename TValue>
-std::optional<std::vector<int>> DijkstraShortestPaths<TGraph<TValue>, TValue>::TryGetShortestPathTo(int destination) const
+std::optional<std::vector<int>> DijkstraShortestPaths<TGraph, TValue>::TryGetShortestPathTo(int destination) const
 {
 	return std::nullopt;
 }
