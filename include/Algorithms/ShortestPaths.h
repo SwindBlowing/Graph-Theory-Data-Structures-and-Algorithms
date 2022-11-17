dfb32b45c074a99b10217db94b8661953c0de838
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-17 22:41:31 +0800

    compile

--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -8,8 +8,8 @@
 
 template <typename TGraph>
 class ShortestPaths {
-  //static_assert(std::is_default_constructible_v<TValue>, 
-	//	"TValue requires default constructor");
+  static_assert(std::is_default_constructible_v<TValue>, 
+		"TValue requires default constructor");
  public:
   ShortestPaths() = delete;
   ShortestPaths(const TGraph *graph, int source) {};
