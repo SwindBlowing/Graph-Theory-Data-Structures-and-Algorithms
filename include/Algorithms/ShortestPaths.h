2da3d2e0a4d25c6f05f3d10d317f08ed9172049d
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-17 17:57:22 +0800

    compile

--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -15,7 +15,7 @@ class ShortestPaths {
   ShortestPaths() = delete;
   ShortestPaths(const TGraph *graph, int source) 
   {
-	static_assert(std::is_constructible_v<TValue>, 
+	static_assert(std::_Enable_default_constructor_tag<TValue>, 
 		"TValue requires default constructor");
   }
   virtual ~ShortestPaths() {}
