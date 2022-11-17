c4254003ea4ed3343069bc03caa0a3a45e2b6bcb
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-17 19:30:15 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -76,7 +76,7 @@ static void test1() {
 struct Test {
 	int x = 0;
 	Test(int i) ;
-	Test() = delete;
+	//Test() = delete;
 	Test operator+(const Test& t) const;
 	bool operator<(const Test& t) const;
 };
