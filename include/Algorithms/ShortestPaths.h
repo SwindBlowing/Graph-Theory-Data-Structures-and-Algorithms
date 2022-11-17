a966806b4921d26aa9bbb8ffcd015cab4e82c311
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-17 16:29:21 +0800

    compile

--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -7,7 +7,7 @@
 template <template<typename> class TGraph>
 class ShortestPaths {
  public:
-  typename TGraph<typename>::value_type temp;
+  typename TGraph<int>::value_type temp;
   typedef bool (ShortestPaths::*fp_Has)(int destination) const;
   typedef std::optional<TValue> (ShortestPaths::*fp_Dis)(int destination) const;
   typedef std::optional<std::vector<int>> (ShortestPaths::*fp_Path)(int destination) const;
