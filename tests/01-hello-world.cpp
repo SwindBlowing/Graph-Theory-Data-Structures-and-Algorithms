dc58468cbcb63c4d9afe976bd7c6eb54da826cd2
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-17 17:45:21 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -34,15 +34,15 @@ static void test1() {
   g->AddEdge(5, 6, 5);
   g->AddEdge(6, 5, 6);
 
-  ShortestPaths<WeightedGraph, int> *p = nullptr;
+  ShortestPaths<WeightedGraph<int>> *p = nullptr;
   for (int i = 1; i <= 6; ++i) {
-    p = new BellmanFordShortestPaths<WeightedGraph, int>(g, i);
+    p = new BellmanFordShortestPaths<WeightedGraph<int>>(g, i);
     for (int j = 1; j <= 6; ++j) {
 	  if (!p->HasPathTo(j)) printf("%d ", -1);
 	  else printf("%d ", p->TryGetDistanceTo(j).value());
     }
     printf("\n");
-	p = new BellmanFordShortestPaths<WeightedGraph, int>(g, 1);
+	p = new BellmanFordShortestPaths<WeightedGraph<int>>(g, 1);
 	std::vector <int> now = p->TryGetShortestPathTo(4).value();
 	for (int i = 0; i < now.size(); i++)
 		printf("%d ", now[i]);
