44b3f8249318100baad85833dea5058061d7d921
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-24 14:40:48 +0800

    compile

--- include/Algorithms/FloydShortestPaths.h
+++ include/Algorithms/FloydShortestPaths.h
@@ -26,18 +26,18 @@ FloydShortestPaths<TGraph>::FloydShortestPaths(const TGraph *graph) : MultiSourc
 	std::vector <WeightedEdge<TValue>> edges = graph->GetEdges();
 	for (int ki = 0; ki < nodes.size(); ki++) {
 		int k = nodes[ki];
-		this->reached[k].insert(std::pair<int, bool> {k, 1});
+		this->reached[k][k] = 1;
 		TValue newTValue = TValue();
-		this->dist[k].insert(std::pair<int, TValue> {k, newTValue});
-		this->preCode[k].insert(std::pair<int, std::optional<int>> {k, std::nullopt});
+		this->dist[k][k] = newTValue;
+		this->preCode[k][k] = std::nullopt;
 	}
 	for (int k = 0; k < edges.size(); k++) {
 		int i = edges[k].GetSource(), j = edges[k].GetDestination();
 		TValue w = edges[k].GetWeight();
-		if (this->reached[i].find(j) == this->reached[i].end() || w < this->dist[i].at(j)) {
-			this->reached[i].insert(std::pair<int, bool> {j, 1});
-			this->dist[i].insert(std::pair<int, TValue> {j, w});
-			this->preCode[i].insert(std::pair<int, std::optional<int>> {j, i});
+		if (this->reached[i].find(j) == this->reached[i].end() || w < this->dist[i][j]) {
+			this->reached[i][j] = 1;
+			this->dist[i][j] = w;
+			this->preCode[i][j] = i;
 		}
 	}
 
@@ -49,10 +49,8 @@ FloydShortestPaths<TGraph>::FloydShortestPaths(const TGraph *graph) : MultiSourc
 				int j = nodes[ji];
 				if (this->reached[k].find(j) == this->reached[k].end()) continue;
 				if (this->reached[i].find(j) == this->reached[i].end()
-					|| this->dist[i].at(k) + this->dist[k].at(j) < this->dist[i].at(j) ) {
-						if (this->reached[i].find(j) == this->reached[i].end())
-							this->dist[i].insert(std::pair<int, TValue> {j, this->dist[i].at(k) + this->dist[k].at(j)});
-						else this->dist[i].find(j)->second = this->dist[i].at(k) + this->dist[k].at(j);
+					|| this->dist[i][k] + this->dist[k][j] < this->dist[i][j]) {
+						this->dist[i][j] = this->dist[i][k] + this->dist[k][j];
 						this->preCode[i][j] = k;
 						this->reached[i][j] = 1;
 					}
